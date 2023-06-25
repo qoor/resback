94159a53e2eff7aaa5052914452f4473e1852c25
MaBling <akck0918@gmail.com>
2023-06-26 01:36:00 +0900

    Add new basic types `Result<>` and `ErrorResponse`

---
+++ src/error.rs
@@ -0,0 +1,10 @@
+use axum::{http::StatusCode, Json};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct ErrorResponse {
+    pub status: &'static str,
+    pub message: String,
+}
+
+pub type Result<T> = std::result::Result<T, (StatusCode, Json<ErrorResponse>)>;
